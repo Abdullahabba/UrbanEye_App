a95ad3ebc91c4bb7db46d5ee011bff7193ac915c
haniumkanwal79-code <haniumkanwal79@gmail.com>
2026-07-29 17:19:51 -0400

    Update login.py

--- auth/login.py
+++ auth/login.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from database.supabase_client import supabase, supabase_admin
 
+def load_css():
+    with open("style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css()
+
 def render_login_page():
     st.title("👁️ Urban Eye AI - Security Portal")
 
